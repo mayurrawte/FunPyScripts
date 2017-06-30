bb03266258cc2f67aee42e21e122c6448c251d15
Mayur Rawte <m.r.rawte7@gmail.com>
2017-07-01 02:00:50 +0530

    Fixed recursive duplicate song download issue

--- YoutubeRelatedSongs.py
+++ YoutubeRelatedSongs.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-from subprocess import call
-
+from subprocess import call, Popen, PIPE
+import os
+import random
 url = raw_input("Initial Youtube song url ")
 noOfSongs = int(raw_input("Enter the number of Related songs to download : "))
 
@@ -10,10 +11,23 @@ while noOfSongs:
 	call(command.split(), shell=False)
 	page = requests.get(url)
 	soup = BeautifulSoup(page.content, 'html.parser')
-	resultset = soup.find_all(class_='autoplay-bar')
-	for a in resultset:
-		link = 'http://www.youtube.com'
-		link += a.find('a').get('href')
-	url = link
+	command1 = "youtube-dl --get-filename "+url
+	p = Popen(command1.split(), stdout=PIPE, stderr=PIPE)
+	output, err = p.communicate()
+	filename = output[:-1]
+	filename = str(filename.replace(filename[-3:],'mp3'))
+	files = os.listdir(os.getcwd())
+	if filename in files:
+		resultset = soup.find(id='watch-related')
+		links = resultset.find_all('a')
+		link = 'http://www.youtube.com'+links[random.randrange(2,len(links)-1)].get('href')
+		url = link
+	else:
+		resultset = soup.find_all(class_='autoplay-bar')
+		for a in resultset:
+			link = 'http://www.youtube.com'
+			link += a.find('a').get('href')
+		url = link
 	noOfSongs-=1
+
 
